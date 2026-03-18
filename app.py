c56b996bdc2d73cebc3ac0949629ca032c0a2fee
befa97435 <fab.bourice@gmail.com>
2026-03-18 08:00:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -83,5 +83,7 @@ def extraire_courses(data):
     return courses
 
 
+import os
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
